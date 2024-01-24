95e082b2e4d389018aa0fd4aefafb3d42489fc61
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-01-24 18:52:05 +0200

    Add get functions

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Defines Caching Class"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -16,3 +16,35 @@ class Cache:
         key = uuid.uuid4().__str__()
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """
+        Gets a value from redis and
+        convert the data back to the desired format.
+        Args:
+            key (str): The key to the stored value
+            callable (function): Converts the value to the desired format
+        """
+        data = self._redis.get(key)
+        if fn is not None:
+            return fn(data)
+        else:
+            return data
+
+    def get_str(self, key: str):
+        """
+        Gets a value from redis and
+        convert the data to str format.
+        Args:
+            key (str): The key to the stored value
+        """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str):
+        """
+        Gets a value from redis and
+        convert the data to int format.
+        Args:
+            key (str): The key to the stored value
+        """
+        return self.get(key, int)
